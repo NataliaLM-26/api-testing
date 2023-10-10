5d09c3a3710579c958a07ed29c080419afed98f6
NataliaLM-26 <t.natalialm@gmail.com>
2023-10-10 12:06:08 -0600

    añade archivos json

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ app.options('*', cors());
 const port = 8080;
 
 app.get('/', (req, res, next) => {
-	res.send('node express api <br> by NataliaLM');
+	res.send('testing api <br> by NataliaLM');
 });
 
-app.get('/work_experience', (req, res) => {
-	res.sendfile("assets/work_experience.json");
+app.get('/proxy_size', (req, res) => {
+	res.sendfile("assets/proxy_size.json");
   });
 
-app.get('/skills', (req, res) => {
-	res.sendfile("assets/skills.json");
+app.get('/dev_hours', (req, res) => {
+	res.sendfile("assets/dev_hours.json");
   });
 
 app.listen(port,  () => 
